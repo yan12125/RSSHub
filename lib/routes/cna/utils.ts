b16a98ef1c98f91d0ef1d4da76730907cbecaa2b
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2026-05-23 18:43:55 +0800

    feat(cna): improve RSS item descriptions
    Add back links for related articles, corresponding to 延伸閱讀 section
    on web pages. Those were removed in the following PR as "those content
    aren't the main article":
    https://github.com/DIYgod/RSSHub/pull/21962.
    
    Note that for those links, some rss readers show all links together, not
    separated by spaces or line breaks, and thus hard to read and confusing.
    For example, without div wrapping, such links from
    https://www.cna.com.tw/news/aipl/202605070117.aspx may be shown as
    立院初審選罷法修正放寬資格　「受緩刑宣告者」可參選18歲公民權要修憲或修法　立院公聽會學者意見不一青年基本法三讀　學團肯定制度上保障18歲參政權
    
    Wrapping them in fiv makes each link in its own line, similar to the web
    page:
    立院初審選罷法修正放寬資格　「受緩刑宣告者」可參選
    18歲公民權要修憲或修法　立院公聽會學者意見不一
    青年基本法三讀　學團肯定制度上保障18歲參政權

--- lib/routes/cna/utils.ts
+++ lib/routes/cna/utils.ts
@@ -18,8 +18,10 @@ export async function getFullText(item) {
     // Those boxes are for explaining terms. They are injected inline and interrupt reading.
     // If readers want to learn about terms, they can learn more online.
     content('.dictionary-box').remove();
-    // Those are for "延伸閱讀" links, which are not part of the main article.
-    content('.moreArticle').remove();
+    // Those are for separating "延伸閱讀" links. On web pages, those links have style "display:block;".
+    // However, some RSS readers, such as TT-RSS, sanitize HTML aggressively and remove <style> tags and style= attributes.
+    // As a result, a wrapping div is needed.
+    content('.moreArticle-link').wrap('<div></div>');
 
     const topImage = content('.fullPic').html();
 
